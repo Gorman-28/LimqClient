8466fbba6a17806601d712e1abdef605dd1418de
Gorman9 <113512486+Gorman9@users.noreply.github.com>
2023-02-24 00:22:52 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using LimqClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LimqClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
